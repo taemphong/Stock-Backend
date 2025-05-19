7355ee3b01a832d9580e25a85584113c0568ff69
Boatts <boatseed159@gmail.com>
2025-05-19 17:39:36 +0700

    product

--- src/report/report.routes.js
+++ src/report/report.routes.js
@@ -8,8 +8,6 @@ const router = Router();
 router.get("/report-add-product", reportController.ReportController);
 router.post("/report-reserve", reportController.ReportReserveController);
 router.get("/peiod-widthdraw", reportController.getMonthOfWidthdrawController);
-
-//***************Intern************************************* */
 router.get("/report-room-withdraw", reportController.reportroomwithdrawController);
 router.post("/report-product-type", reportController.ReportProductTypeController);
 router.post("/report-withdraw-room", reportController.reportroomwithdrawController);
